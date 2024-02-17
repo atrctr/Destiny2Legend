70f1e2ff76af06bd0460a0974df2530692a533ac
Kacper Kowalczyk <kkkowalczyk@pm.me>
2024-02-17 15:09:21 +0000

    Improve layout and styling

--- src/playerprofile.ts
+++ src/playerprofile.ts
@@ -13,26 +13,34 @@ export const playerProfile = ( apiResponse ) => {
     const triumphScoreTotal = apiResponse.Response.metrics.data.metrics['3329916678'].objectiveProgress.progress
 
     let output = `
-            <div class='grid-span-4 grid-start-col2'><h1 class='player-bungiename'>${bungieName}</h1></div>
+            <div class='grid-span-3 grid-start-col2'><h1 class='player-bungiename'>${bungieName}</h1></div>
  
-            <div class='grid-tile grid-span-2 grid-start-col2'>
-                <p><span class="material-icons">sports_esports</span> Playing on ${platformIcons(profileData.userInfo.applicableMembershipTypes)}</p>
+            <div class='grid-tile text-lg'>
+                <p>${platformIcons(profileData.userInfo.applicableMembershipTypes)}</p>
             </div>
 
-            <div class='grid-tile grid-span-2'>
-                <p><span class="material-icons">history</span> Last played: ${lastActiveDate} </p>
+            <div class='grid-tile grid-start-col2'>
+                <p class='text-lg guardian-rank'>
+                    <span class='guardian-rank-roundel'>${profileData.currentGuardianRank}</span>&nbsp;${guardianRanks[profileData.currentGuardianRank]}
+                </p>
+                <p>Current Guardian Rank</p>
             </div>
 
-            <div class='grid-tile grid-span-2 grid-start-col2'><h3>Guardian rank</h3>
-                <p><img height=16px src='/images/icon_guardian_ranks.png' class='icon'> Current rank: 
-                    <span class='guardian-rank'><span class='guardian-rank-roundel'>${profileData.currentGuardianRank}</span> <span class='dimmed'>${guardianRanks[profileData.currentGuardianRank]}</span></span> </p>
-                <p class="dimmed"><span class="material-icons">timeline</span> Highest achieved: 
-                    <span class='guardian-rank'><span class='guardian-rank-roundel'>${profileData.lifetimeHighestGuardianRank}</span> ${guardianRanks[profileData.lifetimeHighestGuardianRank]} </span></p>
+            <div class='grid-tile dimmed'>
+                <p class="text-lg guardian-rank text-lg" >
+                    <span class='guardian-rank-roundel'>${profileData.lifetimeHighestGuardianRank}</span>&nbsp;${guardianRanks[profileData.lifetimeHighestGuardianRank]}
+                </p>
+                <p class="dimmed">Highest Rank achieved</p>
             </div>
 
-            <div class='grid-tile grid-span-2'><h3>Triumph Score</h3>
-                <p><img height=20px src='/images/icon_triumphslifetime.webp' class='icon' /> Active Triumph score: ${triumphScoreActive.toLocaleString()} </p>
-                <p class="dimmed"><img height=20px src='/images/icon_triumphsmemorialized.webp' class='icon' /> Lifetime Triumph score: ${triumphScoreTotal.toLocaleString()} </p>
+            <div class='grid-tile'>
+                <p class='text-lg'><img height=20px src='/images/icon_triumphslifetime.webp' class='icon' /> ${triumphScoreActive.toLocaleString()} </p>
+                <p>Active Triumph score</p>
+            </div>
+
+            <div class='grid-tile'>
+                <p class="dimmed text-lg"><img height=20px src='/images/icon_triumphsmemorialized.webp' class='icon' /> ${triumphScoreTotal.toLocaleString()} </p>
+                <p class="dimmed">Lifetime Triumph score</p>
             </div>`
         
         // ${ playerOwnership(profileData.versionsOwned,profileData.seasonHashes )}    
